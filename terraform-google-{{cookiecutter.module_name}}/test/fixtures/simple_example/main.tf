637a489de0ad23024fb05f7ebb180de3dd6f543d
Aaron Lane <aarondrewlane@gmail.com>
2019-04-17 18:58:24 -0400

    Simplify example fixture

--- terraform-google-{{cookiecutter.module_name}}/test/fixtures/simple_example/main.tf
+++ terraform-google-{{cookiecutter.module_name}}/test/fixtures/simple_example/main.tf
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
-resource "random_string" "random_suffix" {
-  length  = 4
-  upper   = "false"
-  lower   = "true"
-  number  = "false"
-  special = "false"
+provider "random" {
+  version = "~> 2.0"
+}
+
+resource "random_pet" "main" {
+  length    = 1
+  prefix    = "simple-example"
+  separator = "-"
 }
 
 module "example" {
   source = "../../../examples/simple_example"
 
   project_id  = "${var.project_id}"
-  bucket_name = "simple-example-${random_string.random_suffix.result}"
+  bucket_name = "${random_pet.main.result}"
 }
