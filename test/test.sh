6b1775eaa884d4138de61e74963dc50d561419b2
Aaron Lane <aarondrewlane@gmail.com>
2019-04-24 14:27:25 -0400

    Drop obsolete references to shared variables file

--- test/test.sh
+++ test/test.sh
@@ -11,8 +11,11 @@ cookiecutter --no-input "${_curdir}" module_name=module-test
 cd ./terraform-google-module-test || exit 1
 cp "${_curdir}/credentials.test.json" ./credentials.json
 _project_id=$(cat credentials.json | jq -r '.project_id')
-tee ./test/fixtures/shared/terraform.tfvars <<EOF
-project_id="${_project_id}"
+tee ./kitchen.local.yml <<EOF
+---
+driver:
+  variables:
+    project_id: ${_project_id}
 EOF
 make test_integration_docker
 _result=$?
