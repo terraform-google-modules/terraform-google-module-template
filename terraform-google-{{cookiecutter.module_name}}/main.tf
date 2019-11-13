4752485056bf9de8f4cefc259a9333996d0da6e9
Morgante Pell <morgantep@google.com>
2019-11-13 18:15:10 -0500

    Remove redundant required_version from main.tf

--- terraform-google-{{cookiecutter.module_name}}/main.tf
+++ terraform-google-{{cookiecutter.module_name}}/main.tf
@@ -14,10 +14,6 @@
  * limitations under the License.
  */
 
-terraform {
-  required_version = "~> 0.12.0"
-}
-
 resource "google_storage_bucket" "main" {
   project = var.project_id
   name    = var.bucket_name
